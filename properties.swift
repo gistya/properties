d7d15dd1f45b522cc7dcbae7370e780422d05620
Jon Gilbert <gistya@users.noreply.github.com>
2017-12-21 08:01:06 -0800

    initial working PoC
    step 3

--- properties.swift
+++ properties.swift
@@ -1,306 +1,224 @@
 
-postfix operator …
-postfix func … <T, R>(key: T) -> R where T: RawRepresentable, R == T.RawValue {
-    return key.rawValue
-}
-
 // MARK: - Property Protocols
+
+import Foundation
 
 /// Can represent any property.
 ///
 /// - Type KP: the type-erased KeyPath of the property.
-///            Can be downcast.
+///            Can be downcast.
 ///
 /// - var keypath: AnyKeyPath
 ///
-protocol AnyPropertyProtocol: Any
-where KP: AnyKeyPath, Root: Any, Value: Any {
-    associatedtype Root
-    associatedtype Value
-    associatedtype KP
+protocol AnyPropertyProtocol {
+    associatedtype Root = Any
+    associatedtype Value = Any?
+    associatedtype KP: AnyKeyPath
     var key: KP { get }
     var value: Value { get }
 }
 
 /// Can represent a property with a specific root.
 ///
-/// - type Root  : the type the property belongs to
-/// - type KP    : the partially type-erased KeyPath of the property.
-///                Can be downcast.
+/// - type Root  : the type the property belongs to
+/// - type KP    : the partially type-erased KeyPath of the property.
+///                Can be downcast.
 ///
 /// - var keypath: PartialKeyPath<Root>
 ///
 protocol PartialPropertyProtocol: AnyPropertyProtocol
 where KP: PartialKeyPath<Root> {
-    init(key: KP, value: Value)
-}
-extension PartialPropertyProtocol {
-    static func property(key: KP, value: Value) -> Self {
-        return Self(key: key, value: value)
-    }
+    
 }
 
-/// Can represent a property with a specific root.
-///
-/// - type Root  : the type the property belongs to
-/// - type KP    : the KeyPath of the property.
-/// - type Value : the type of the property's value
-///
-/// - var keypath: KeyPath<Root, Value>
-///
 protocol PropertyProtocol: PartialPropertyProtocol
 where KP: KeyPath<Root, Value> {
     init(key: KP, value: Value)
 }
-extension PropertyProtocol {
-    static func property(key: KP, value: Value) -> Self {
-        return Self(key: key, value: value)
-    }
-}
 
-/// Can represent a property with a specific root.
-///
-/// - type Root  : the type the property belongs to
-/// - type KP    : the WritableKeyPath of the property.
-/// - type Value : the type of the property's value
-///
-/// - var keypath: WritableKeyPath<Root, Value>
-///
-protocol WritablePropertyProtocol: PropertyProtocol
-where KP: WritableKeyPath<Root, Value> {}
-
-/// Can represent a property with a specific root.
-///
-/// - type Root  : the type the property belongs to
-/// - type KP    : the partially ReferenceWritableKeyPath of the property.
-/// - type Value : the type of the property's value
-///
-/// - var keypath: ReferenceWritableKeyPath<Root, Value>
-///
-protocol ReferenceWritablePropertyProtocol: PropertyProtocol
-where KP: ReferenceWritableKeyPath<Root, Value> {}
-
-// MARK: - Array Extension
-
-extension Array
-where Element: AnyPropertyProtocol {
-    ///
-    func firstValue<R, V>(for keyPath: KeyPath<R,V>) -> V? where Element.Root == R {
-        guard let value: V = (self.first { prop in prop.key == keyPath })?.value as? V else {
-            return nil
-        }
-        return value
-    }
-}
-
-/// Proof of concept
-struct Foo {
-    let bar: String
-    let baz: Int
-    init?<P>(properties: [P]) where P: AnyPropertyProtocol, P.Root == Foo {
-        guard 
-            let bar = properties.firstValue(for: \Foo.bar),
-            let baz = properties.firstValue(for: \Foo.baz)
-            else {
-                return nil
-        }
-        self.bar = bar
-        self.baz = baz
-    }
-}
-
-struct Bar {
-    var keys: [AnyKeyPath] = [\Bar.foo, \Bar.baz]
-    let types = ["", 0] as [Any]
-    var foo: String = ""
-    var baz: Int = 0
-    //func asdf<KP, P: AnyPropertyProtocol>(kp: KP, p: P) where KP: KeyPath<
-    init?<P>(properties: [P]) where P: PartialPropertyProtocol, P.Root == Bar {
-        print(keys)
-        for (key, property) in zip(keys, properties) where key == property.key {
-            //guard let prop = (properties.first { $0.key == key && type(of: $0.key) == type(of: key) })
-            guard let val = properties.firstValue(for: key)
-                else {
-                    print("fail")
-                    continue
-            }
-            print(prop.key)
-            let nk = prop.key
-            let r = type(of: nk).rootType 
-            let v = type(of: nk).valueType
-            print(v)
-            let k = types.first { type(of: $0) == v }
-            func s<H>(_ g:H) {
-                print(H.self)
-            }
-            s(k!)
-            print(nk)
-            //self[keyPath:nk] = prop.value 
-            func encode<F>(_ value: F, forKey: WritableKeyPath<Bar,F>) {
-                
-            }
-        }
-        return nil
-    }
-}
-
-let kpTypes = [KeyPath<Bar,String>.self, KeyPath<Bar,Int>.self]
-let kps = [\Bar.foo, \Bar.baz]
-let types = [String.self, Int.self] as [Any]
-
-var debug = ""
-
-func typify<PKP, R, V>(_ pkp: PKP) -> V? where PKP: PartialKeyPath<R> {
-    guard let kp = pkp as? KeyPath<R,V> else {
-        return nil
-    }
-    if "" is V {
-        print("\(V.self) is string")
-    }
-    else if 0 is V {
-        print("\(V.self) is int")
-    }
-    else {
-        print("\(V.self)")
-    }
-    return nil
-}
-
-for x in kpTypes {
-    debug += "\(type(of: x))\n"
-}
-
-for x in kps {
-    debug += "\(type(of: x))\n"
-    if let str: String = typify(x) {
-        
-    } else if let int: Int = typify(x) {
-        
-    }
-}
-
-print(debug)
-
-struct PartialProperty<R>: PartialPropertyProtocol {
+struct Property<R, V>: PropertyProtocol {
     typealias Root = R
-    typealias KP = PartialKeyPath<R>
-    typealias Value = Any
+    typealias Value = V
+    typealias KP = KeyPath<R, V>
     let key: KP
     let value: Value
     init(key: KP, value: Value) {
         self.key = key
         self.value = value
     }
+    
+    init(_ arg: (key: KP, value: Value)) {
+        self.key = arg.key
+        self.value = arg.value
+    }
+    
+    public static func partial(
+        key: KP,
+        value: Value) -> PartialProperty<Root>
+    {
+        let property = self.init(
+            key: key,
+            value: value
+        )
+        return PartialProperty(property)
+    }
 }
 
-///
-struct Property<R, V>: PropertyProtocol {
+internal protocol _AnyPropertyBox {
+    var key: AnyKeyPath { get }
+    var value: Any? { get }
+    var _base: Any { get }
+}
+
+internal protocol _PartialPropertyBox: _AnyPropertyBox {
+    associatedtype _Root
+}
+
+internal struct _ConcretePartialPropertyBox<Base: PropertyProtocol> {
+    typealias _Root = Base.Root
+    typealias _Value = Base.Value
+    internal var _baseProperty: Base
+}
+
+extension _ConcretePartialPropertyBox: _PartialPropertyBox {
+    internal var _base: Any { return _baseProperty }
+    
+    var key: AnyKeyPath {
+        return _baseProperty.key
+    }
+    
+    internal var value: Any? {
+        get {
+            return _baseProperty.value as Any?
+        }
+    }
+    
+    internal init(_ base: Base) {
+        self._baseProperty = base
+    }
+}
+
+ struct PartialProperty<R>: PartialPropertyProtocol {
+    typealias Value = Any?
+    typealias KP = PartialKeyPath<R>
     typealias Root = R
-    typealias Value = V
-    typealias KP = KeyPath<R,V>
-    let key: KP
-    let value: V
+    
+    var key: PartialKeyPath<R>
+    var value: Value
+    
+    internal var _box: _AnyPropertyBox
+
+    init<V>(_ base: Property<Root, V>) {
+        self.value = base.value
+        self.key = base.key as! PartialKeyPath<Root>
+        self._box = _ConcretePartialPropertyBox<Property<Root, V>>(base)
+    }
+    public var unBoxed: Any { return _box._base }
 }
 
-let props = [PartialProperty(key: \Bar.foo, value: "foo"), PartialProperty(key: \Bar.baz, value: 1337)]
 
-let bar = Bar(properties: props)
-
-
-// MARK: - Other array extension funcs 
+typealias Properties<R> = Array<PartialProperty<R>>
 
 extension Array
-    where Element: AnyPropertyProtocol 
-{
-    /// 
-    func properties<R, V, A>() -> [A]
-        where
-        Element: AnyPropertyProtocol,
-        A: PropertyProtocol,
-        A.Root == R, A.Value == V
-    {
-        var propertyArray = [A]()
-        for element in self {
-            propertyArray.append(A.property(
-                key: element.key as! A.KP,
-                value: element.value as! V
-            ))
-        }
-        return propertyArray
+where Element: AnyPropertyProtocol {
+    ///
+    func firstValue<R, V>(for key: KeyPath<R,V>, default rest: R?) -> V?
+        where Element.Root == R {
+            return first { $0.key == key }?.value as? V ?? rest?[keyPath: key]
     }
     
     ///
-    func partialProperties<R, A>() -> [A]
-        where Element.Root == R,
-        A: PartialPropertyProtocol,
-        A.Root == R, A.Value == Any
-    {
-        var propertyArray = [A]()
-        for element in self {
-            propertyArray.append(A.property(
-                key: element.key as! A.KP, value: element.value
-            ))
-        }
-        return propertyArray
+    func firstOptionalValue<R, V>(for key: KeyPath<R,Optional<V>>, default rest: R?) -> V?
+        where Element.Root == R {
+            return first { $0.key == key }?.value as? V ?? rest?[keyPath: key]
     }
 }
 
-// MARK: - Initializability Protocols
+protocol PropertyInitializable {
+    ///
+    init?(with properties: [PartialProperty<Self>], copying rest: Self?)
+}
 
-///
-///
-///
-protocol InitializableWithProperties {}
-//    associatedtype PSI: PropertiesSufficientForInitializationProtocol where PSI.Root == Self
-//    static var propertiesSufficientForInitialization: PSI { get }
-//    init?<PSI>(_ properties: PSI)
-//extension InitializableWithProperties {
-//    init?<AnyProperty>(_ properties: [AnyProperty])
-//    where AnyProperty: AnyPropertyProtocol {
-//        for property in properties {
-//            self[keyPath: property.key] = property.value
-//        }
-//        return nil
-//    }
-//}
-//class AnyProperty: AnyPropertyProtocol {
-//
-//}
-//
-//
-//infix operator √
-//prefix func √<R: AnyKeyPath>(left: R, right: Any) -> AnyProperty {
-//    return root
-//}
-//
-//let x = √\Foo.bar
-//print(x)
-//typealias Properties<T> = [PartialPropertyProtocol] where T == PartialPropertyProtocol.Root
-/// A container for a set of properties that are sufficient to initialize the Root type.
-///
-/// - type Root  : the type the property belongs to
-///
-//protocol PropertiesSufficientForInitializationProtocol {
-//    associatedtype Root
-//    associatedtype PP = PartialPropertyProtocol where PP.Root == Root
-//    var sufficientForInitialization: [PP] { get }
-//}
-//
-//struct Properties<R, P: PartialPropertyProtocol>: PropertiesSufficientForInitializationProtocol {
-//    typealias Root = R
-//    var sufficientForInitialization: [P]
-//}
-//struct ProofOfConcept: InitializableWithProperties {
-//    typealias PSI = Properties
-//    static var propertiesSufficientForInitialization: PSI = Properties()
-//    let foo: String
-//    let bar: Int
-//    init?(_ properties: PSI) throws {
-//        for property in properties.sufficientForInitialization {
-//
-//        }
-//    }
-//}
-/// Allows you to access the rawValue by appending * to the case.
-/// e.g.: .CaseName* == .CaseName.rawValue
-/// This allows cleaning up the call site and reducing boilerplate :D
+extension Optional where Wrapped: PropertyInitializable {
+    ///
+    static func ||<Value>(
+        lhs: ([PartialProperty<Wrapped>], KeyPath<Wrapped, Value>),
+        rhs: Wrapped?)
+        -> Value?
+    {
+        return lhs.0.firstValue(for: lhs.1, default: rhs)
+    }
+    
+    ///
+    static func ||<Value>(
+        lhs: ([PartialProperty<Wrapped>], KeyPath<Wrapped, Value?>),
+        rhs: Wrapped?)
+        -> Value?
+    {
+        return lhs.0.firstValue(for: lhs.1, default: rhs)!
+    }
+}
+
+struct Test: PropertyInitializable {
+    let str1: String
+    let str2: String
+    let str3: String?
+    let str4: String?
+    let int1: Int
+    let int2: Int
+    let int3: Int?
+    let int4: Int?
+    
+    init?(with properties: [PartialProperty<Test>], copying rest: Test?) {
+        // Init non-optional properties using a guard statement.
+        guard let str1 = (properties, \.str1) || rest,
+            let str2 = (properties, \.str2) || rest,
+            let int1 = (properties, \.int1) || rest,
+            let int2 = (properties, \.int2) || rest
+            else { return nil }
+        self.str1 = str1
+        self.str2 = str2
+        self.int1 = int1
+        self.int2 = int2
+        
+        // Optional properties can simply be assigned.
+        self.str3 = (properties, \.str3) || rest
+        self.str4 = (properties, \.str4) || rest
+        self.int3 = (properties, \.int3) || rest
+        self.int4 = (properties, \.int4) || rest
+    }
+}
+
+var properties1: Properties<Test> = [
+    Property.partial(key: \Test.str1, value: "1"),
+    Property.partial(key: \Test.str2, value: "2"),
+    Property.partial(key: \Test.str3, value: "3"),
+    Property.partial(key: \Test.str4, value: nil),
+    Property.partial(key: \Test.int1, value: 1),
+    Property.partial(key: \Test.int2, value: 2),
+    Property.partial(key: \Test.int3, value: 3),
+    Property.partial(key: \Test.int4, value: nil)
+]
+
+var properties2: [PartialProperty<Test>] = [
+    Property.partial(key: \Test.str1, value: "foo"),
+    Property.partial(key: \Test.str3, value: "bar"),
+    Property.partial(key: \Test.str4, value: "baz"),
+    Property.partial(key: \Test.int3, value: 1300),
+    Property.partial(key: \Test.int4, value: 37)
+]
+
+let test1 = Test(with: properties1, copying: nil)
+
+assert(test1 != nil, "test1 should not be nil.")
+
+let test2 = Test(with: properties2, copying: test1)
+
+assert(test2 != nil, "test2 should not be nil.")
+assert(test2!.int3! + test2!.int4! == 1337, "test2 should be l33t.")
+
+print(test2!.int3! + test2!.int4!)
+
+let test3 = Test(with: properties2, copying: nil)
+
+assert(test3 == nil, "test3 should be nil.")
